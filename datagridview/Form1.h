d3639a39e2229d434f2380a7eb7b42185e086386
Remo Fiorentino <rfiorentino@uninorte.edu.co>
2016-03-25 17:25:03 -0500

    first commit

---
+++ datagridview/Form1.h
@@ -0,0 +1,102 @@
+#pragma once
+
+namespace datagridview {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén utilizando.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::DataGridView^  dataGridView1;
+	private:
+		/// <summary>
+		/// Variable del diseñador requerida.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido del método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->CellBorderStyle = System::Windows::Forms::DataGridViewCellBorderStyle::Raised;
+			this->dataGridView1->ColumnHeadersBorderStyle = System::Windows::Forms::DataGridViewHeaderBorderStyle::Single;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->ColumnHeadersVisible = false;
+			this->dataGridView1->Location = System::Drawing::Point(12, 12);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersVisible = false;
+			this->dataGridView1->Size = System::Drawing::Size(406, 316);
+			this->dataGridView1->TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(430, 340);
+			this->Controls->Add(this->dataGridView1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
+				 Bitmap^ img;
+				 DataGridViewImageColumn^ col;
+				 for(int i=0;i<2;i++){
+					img = gcnew Bitmap("meme1.bmp");
+					col = gcnew System::Windows::Forms::DataGridViewImageColumn();
+					col->Name="col"+i;
+					col->ImageLayout = System::Windows::Forms::DataGridViewImageCellLayout::Stretch;
+					dataGridView1->Columns->Add(col);
+				 }
+				 while(dataGridView1->RowCount<2){
+					 dataGridView1->Rows->Add();
+				 }
+				 for(int i=0;i<2;i++){
+					 for(int j=0;j<2;j++){
+						 dataGridView1->Rows[i]->Cells[j]->Value = img;
+					 }
+				 }
+			 }
+};
+}
+
